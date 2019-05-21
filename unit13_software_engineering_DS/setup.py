d4a20d981dfbe0c3efc126397223e587f5e6cbd1
Joe Earnshaw <jearnshaw58@gmail.com>
2019-05-21 12:56:53 -0700

    refactor exploratory analysis of passenger survival predictions using ridge logistic regression

--- unit13_software_engineering_DS/setup.py
+++ unit13_software_engineering_DS/setup.py
@@ -26,6 +26,15 @@ setup(
     license='MIT',
     packages=['titanic'],
     install_requires=[
-        'pypandoc>=1.4'
-    ]
+        'pypandoc>=1.4',
+        'watermark>=1.8.1',
+        'pandas>=0.24.2',
+        'scikit-learn>=0.20.3',
+        'scipy>=1.2.1',
+        'matplotlib>=3.0.3',
+        'pytest>=4.3.1',
+        'pytest-runner>=4.4'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
 )
