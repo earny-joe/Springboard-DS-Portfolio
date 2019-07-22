d35396ae080b6076c70dbac81359b79ca9013f4a
Joseph <jearnshaw58@gmail.com>
2019-07-22 11:52:02 -0700

    completed 3 RF models in EDA to predict cardiomegaly using CSV file

--- capstone_2/capstone/eda.py
+++ capstone_2/capstone/eda.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_score
+from sklearn.metrics import roc_curve, roc_auc_score
+from imblearn.over_sampling import SMOTE
+
 
 def eda_path(cwd):
     '''create function that defines project path and data path'''
@@ -132,6 +139,169 @@ def uncertain_col(df, pathologies):
     df['uncertain_features'] = df['uncertain_features'].replace('', 'None')
     
     return df
+
+def ml_baseline(X, y, random_state, title):
+    '''function that splits data, fits baseline classifier, cross-validates and then prints out performance metrics'''
+    # Split the data into 70% train and 30% test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    # initiate classifier
+    classifier = RandomForestClassifier(n_estimators=100, random_state=random_state, n_jobs=-1)
+    # fit random forest classifier
+    classifier.fit(X_train, y_train)
+    # cross validate and record performance metrics: roc_auc, precision and recall
+    classifier_scores = cross_validate(classifier, X_train, y_train, cv=5,
+                                   scoring=['roc_auc', 'precision', 'recall'],
+                                   n_jobs=-1)
+    print('Mean ROC-AUC score of test set for {}: {:.3f}'.format(title, classifier_scores['test_roc_auc'].mean()))
+    print('Mean Precision score of test set for {}: {:.3f}'.format(title, classifier_scores['test_precision'].mean()))
+    print('Mean Recall score of test set for {}: {:.3f}'.format(title, classifier_scores['test_recall'].mean()))
+    # create series that stores feature importance from classifier model
+    feature_imp = pd.Series(classifier.feature_importances_, index=X.columns).sort_values(ascending=False)
+    # plot important features
+    plt.figure(figsize=(12,6))
+    sns.barplot(x=feature_imp[:6], y=feature_imp.index[:6], edgecolor='black')
+    plt.xlabel('Feature Importance Score')
+    plt.ylabel('Features')
+    plt.title("Visualizing Important Features")
+    # plot AUROC curve
+    # predict probabilities
+    probs = classifier.predict_proba(X_test)
+    # keep probabilities for positive outcome only
+    probs = probs[:, 1]
+    # calculate roc curve
+    fpr, tpr, thresholds = roc_curve(y_test, probs)
+    #calculate AUC
+    auc = roc_auc_score(y_test, probs)
+    plt.figure(figsize=(12,8))
+    # plot no skill
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    # plot the roc curve for the model
+    plt.plot(fpr, tpr, marker='.')
+    plt.title('AUROC Curve for {} Model'.format(title))
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the plot
+    plt.show()
+    
+    return classifier, classifier_scores
+
+
+def ml_sample(df, random_state, title):
+    '''function that resamples with data splits it, fits random forest classifier, cross-validates and then prints out metrics'''
+    # Class count
+    count_class_0, count_class_1 = df['Cardiomegaly'].value_counts()
+    # Divide by class
+    df_class_0 = df[df['Cardiomegaly'] == 0]
+    df_class_1 = df[df['Cardiomegaly'] == 1]
+    # create over samples data set 
+    df_class_1_over = df_class_1.sample(count_class_0, replace=True)
+    df_test_over = pd.concat([df_class_0, df_class_1_over], axis=0)
+    print('Random over-sampling:')
+    print(df_test_over['Cardiomegaly'].value_counts())
+    print('-' * 40)
+    # plot new value counts for over-sampled data set
+    df_test_over['Cardiomegaly'].value_counts().plot(kind='bar', title='Count (target)', figsize=(12,6), color='steelblue', edgecolor='black')
+    # create dataframe of features and the target variable Cardiomegaly
+    X_sample = df_test_over.drop('Cardiomegaly', axis=1)
+    y_sample = df_test_over['Cardiomegaly']
+    # Split the data into 70% train and 30% test
+    X_train, X_test, y_train, y_test = train_test_split(X_sample, y_sample, test_size=0.3, random_state=1)
+    # initiate classifier
+    classifier = RandomForestClassifier(n_estimators=100, random_state=random_state, n_jobs=-1)
+    # fit random forest classifier to model
+    classifier.fit(X_train, y_train)
+    # cross validate and record performance metrics
+    classifier_scores = cross_validate(classifier, X_train, y_train, cv=5,
+                                   scoring=['roc_auc', 'precision', 'recall'],
+                                   n_jobs=-1)
+    print('Mean ROC-AUC score of test set for {}: {:.3f}'.format(title, classifier_scores['test_roc_auc'].mean()))
+    print('Mean Precision score of test set for {}: {:.3f}'.format(title, classifier_scores['test_precision'].mean()))
+    print('Mean Recall score of test set for {}: {:.3f}'.format(title, classifier_scores['test_recall'].mean()))
+    # create series that stores feature importance from classifier model
+    feature_imp = pd.Series(classifier.feature_importances_, index=X_sample.columns).sort_values(ascending=False)
+    # plot important features
+    plt.figure(figsize=(12,6))
+    sns.barplot(x=feature_imp[:6], y=feature_imp.index[:6], edgecolor='black')
+    plt.xlabel('Feature Importance Score')
+    plt.ylabel('Features')
+    plt.title("Visualizing Important Features")
+    # plot AUROC curve
+    # predict probabilities
+    probs = classifier.predict_proba(X_test)
+    # keep probabilities for positive outcome only
+    probs = probs[:, 1]
+    # calculate roc curve
+    fpr, tpr, thresholds = roc_curve(y_test, probs)
+    #calculate AUC
+    auc = roc_auc_score(y_test, probs)
+    plt.figure(figsize=(12,8))
+    # plot no skill
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    # plot the roc curve for the model
+    plt.plot(fpr, tpr, marker='.')
+    plt.title('AUROC Curve for {} Model'.format(title))
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the plot
+    plt.show()
+    
+    return classifier, classifier_scores
+
+def ml_smote(X, y, random_state, title):
+    '''function that utilizes SMOTE to oversample data set, then splits it, fits random forest classifier, cross-validates
+    and then prints out metrics
+    '''
+    # initiate smote object
+    smote = SMOTE(ratio='minority')
+    # fit and resample the data set using SMOTE
+    X_sm, y_sm = smote.fit_sample(X, y)
+    # Split the data into 70% train and 30% test
+    X_train, X_test, y_train, y_test = train_test_split(X_sm, y_sm, test_size=0.3, random_state=random_state)
+    # initiate classifier
+    classifier = RandomForestClassifier(n_estimators=100, random_state=random_state, n_jobs=-1)
+    # fit random forest classifier to model
+    classifier.fit(X_train, y_train)
+    # cross validate and record performance metrics
+    classifier_scores = cross_validate(classifier, X_train, y_train, cv=5,
+                                   scoring=['roc_auc', 'precision', 'recall'],
+                                   n_jobs=-1)
+    print('Mean ROC-AUC score of test set for {}: {:.3f}'.format(title, classifier_scores['test_roc_auc'].mean()))
+    print('Mean Precision score of test set for {}: {:.3f}'.format(title, classifier_scores['test_precision'].mean()))
+    print('Mean Recall score of test set for {}: {:.3f}'.format(title, classifier_scores['test_recall'].mean()))
+   # create series that stores feature importance from classifier model
+    feature_imp = pd.Series(classifier.feature_importances_, index=X.columns).sort_values(ascending=False)
+    # plot important features
+    plt.figure(figsize=(12,6))
+    sns.barplot(x=feature_imp[:6], y=feature_imp.index[:6], edgecolor='black')
+    plt.xlabel('Feature Importance Score')
+    plt.ylabel('Features')
+    plt.title("Visualizing Important Features")
+    # plot AUROC curve
+    # predict probabilities
+    probs = classifier.predict_proba(X_test)
+    # keep probabilities for positive outcome only
+    probs = probs[:, 1]
+    # calculate roc curve
+    fpr, tpr, thresholds = roc_curve(y_test, probs)
+    #calculate AUC
+    auc = roc_auc_score(y_test, probs)
+    plt.figure(figsize=(12,8))
+    # plot no skill
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    # plot the roc curve for the model
+    plt.plot(fpr, tpr, marker='.')
+    plt.title('AUROC Curve for {} Model'.format(title))
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the plot
+    plt.show()
+    
+    return classifier, classifier_scores
+    
+    
+    
+    
+    
     
     
     
